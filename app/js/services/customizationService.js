e578bef1d74ac67d90522fd81860b2828fc0cab6
charlottelecuit <42929880+charlottelecuit@users.noreply.github.com>
2018-09-04 14:09:29 +0200

    Add customization cd69

--- app/js/services/customizationService.js
+++ app/js/services/customizationService.js
@@ -11,6 +11,8 @@ angular.module('ddsCommon').factory('CustomizationService', function(lyonMetropo
                 return 'D75-PARIS';
             if (testCase.menages._.depcom[currentPeriod].match(/^14/))
                 return 'D14-CALVADOS';
+            if (testCase.menages._.depcom[currentPeriod].match(/^69/))
+                return 'D69-DDR';
             if (_.includes(lyonMetropoleInseeCodes, testCase.menages._.depcom[currentPeriod]))
                 return 'M69-LYON';
         }
