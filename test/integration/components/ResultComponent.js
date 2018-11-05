b81c33bf6134c6101e615772dfcdc6ac4a625a0f
Alexandre Segura <mex.zktk@gmail.com>
2018-11-05 16:32:33 +0100

    Fix Watai test for Firefox.
    - Change selector.
    - Use custom function.

--- test/integration/components/ResultComponent.js
+++ test/integration/components/ResultComponent.js
@@ -2,15 +2,22 @@ title: 'h1',
 
 resultatFrame: '.frame-resultats .droits-eligibles',
 
+prestationTabs: '.droits-eligibles  div[role="tab"]',
 prestationName: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .description',
 prestationMontant: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .montant',
 prestationMontantDetail: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .montant-detail',
 prestationYM2Warning: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') i.fa-warning',
-togglePrestationTabLink: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') a',
+togglePrestationTabLink: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') div[role="tab"]',
 prestationDescription: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') p',
 prestationMoreInfoLink: '.droits-eligibles .result:nth-child(' + PRESTATION_TO_TEST_POSITION + ') .btn-default',
 
-openPrimeActiviteLink: { a: 'Prime d’activité' },
+openPrimeActivite: function() {
+    return this.prestationTabs.then(function (tab) {
+        if (driver.textPresent('Prime d’activité', tab)) {
+            return tab.click();
+        }
+    });
+},
 requestPrimeActiviteLink: { a: 'Faire une demande' },
 
 greyedPrestation: '.droits-eligibles .needs-n-2',
