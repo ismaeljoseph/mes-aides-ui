bd28c786181f91dfaa0a401ffe33ee45f706c089
Thomas Guillet <guillet.thomas@gmail.com>
2018-11-29 14:06:48 +0100

    Allow bulk retrieval

--- backend/lib/openfisca/bulk/index.js
+++ backend/lib/openfisca/bulk/index.js
@@ -12,11 +12,12 @@ var lib = require('..');
 
 var counter = 0;
 var errors = 0;
-var limit = 5;
+var limit = 30000;
 var startDate = (new Date()).toISOString();
 
 
 entityGroups = {
+  individus: [],
   familles: ['parents', 'enfants'],
   foyers_fiscaux: ['declarants', 'personnes_a_charge'],
   menages: ['personne_de_reference', 'conjoint', 'enfants']
@@ -34,27 +35,52 @@ function reset() {
 
 function prefix(prefix, situation) {
   Object.keys(entityGroups).forEach(entityName => {
+    oldKeys = Object.keys(situation[entityName]);
+    oldKeys.forEach(name => {
+      situation[entityName][prefix + name] = situation[entityName][name]
+      delete situation[entityName][name]
+    })
 
+    entityGroups[entityName].forEach(property => {
+      Object.keys(situation[entityName]).forEach(id => {
+        let entity = situation[entityName][id]
+
+        if (entity[property]) {
+          entity[property] =  entity[property].map(id => prefix + id)
+        }
+      })
+    })
   })
 
   return situation
 }
 
+function append(acummulator, situation) {
+  Object.keys(entityGroups).forEach(entityName => {
+    Object.keys(situation[entityName]).forEach(id => {
+      acummulator[entityName][id] = situation[entityName][id]
+    })
+  })
+
+  return acummulator
+}
+
 result = reset()
 function test() {
-    Situation.find().sort({ dateDeValeur: -1 }).limit(limit).cursor()
-        .pipe(es.map(function (situation, done) {
-            console.log(lib.buildOpenFiscaRequest(situation))
-            done()
-        }))
-        .on('end', function() {
-            process.exit();
-        })
-        .on('error', function(err) {
-            console.trace(err);
-            process.exit();
-        })
-        .resume();
+  Situation.find().sort({ dateDeValeur: -1 }).skip(100).limit(limit).cursor()
+    .pipe(es.map(function (situation, done) {
+      append(result, prefix(situation._id.valueOf(), lib.buildOpenFiscaRequest(situation)))
+      done()
+    }))
+    .on('end', function() {
+      console.log(JSON.stringify(result))
+      process.exit();
+    })
+    .on('error', function(err) {
+      console.trace(err);
+      process.exit();
+    })
+    .resume();
 }
 
 test();
