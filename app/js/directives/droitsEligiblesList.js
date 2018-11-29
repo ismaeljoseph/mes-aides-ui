910c1f0be1effc2a77cbaf32d486a810ee64b183
Alexandre Segura <mex.zktk@gmail.com>
2018-11-29 13:56:15 +0100

    Use link function, avoid using $eval.

--- app/js/directives/droitsEligiblesList.js
+++ app/js/directives/droitsEligiblesList.js
@@ -1,22 +1,39 @@
 'use strict';
 
 var controllerOptions = function(templateUrl) {
-    return function() {
+    return function(TrampolineService) {
         return {
             restrict: 'E',
             templateUrl: templateUrl,
-            scope: true,
-            controller: 'droitsEligiblesListCtrl',
-            // Inject list into scope, filtered by benefits specified via the "filter" attribute
+            scope: {
+                list: '='
+            },
+            // Best Practice: use controller when you want to expose an API to other directives. Otherwise use link.
+            // @see https://docs.angularjs.org/guide/directive
             link: function ($scope, $element, $attributes) {
+
+                // Filter by benefit specified via the "filter" attribute
                 if ($attributes.hasOwnProperty('filter')) {
                     var filter = $scope.$eval($attributes.filter);
-                    $scope.list = _.pickBy($scope.$eval($attributes.list), function(value, key) {
+                    $scope.list = _.pickBy($scope.list, function(value, key) {
                         return _.includes(filter, key);
                     });
-                } else {
-                    $scope.list = $scope.$eval($attributes.list);
                 }
+
+                $scope.isNumber = _.isNumber;
+                $scope.isString = _.isString;
+
+                $scope.shouldDisplayYM2Warning = function(droit) {
+                    return droit.isBaseRessourcesYearMoins2 && ! $scope.ressourcesYearMoins2Captured && ! _.isString(droit.montant);
+                };
+
+                $scope.trampoline = TrampolineService;
+
+                // ng-class and uib-accordion don't work well together, hence this extra function.
+                // See https://github.com/angular-ui/bootstrap/issues/4172
+                $scope.getAccordionClass = function(droit) {
+                    return [$scope.shouldDisplayYM2Warning(droit) ? 'needs-n-2' : '', droit.open ? 'panel-opened': ''].join(' ');
+                };
             }
         };
     };
@@ -27,19 +44,3 @@ angular.module('ddsApp')
 
 angular.module('ddsApp')
     .directive('droitNonEligiblesList', controllerOptions('/partials/droits-non-eligibles-list.html'));
-
-angular.module('ddsApp').controller('droitsEligiblesListCtrl', function($scope, TrampolineService) {
-    $scope.isNumber = _.isNumber;
-    $scope.isString = _.isString;
-    $scope.shouldDisplayYM2Warning = function(droit) {
-        return droit.isBaseRessourcesYearMoins2 && ! $scope.ressourcesYearMoins2Captured && ! _.isString(droit.montant);
-    };
-
-    $scope.trampoline = TrampolineService;
-
-    // ng-class and uib-accordion don't work well together, hence this extra function.
-    // See https://github.com/angular-ui/bootstrap/issues/4172
-    $scope.getAccordionClass = function(droit) {
-        return [$scope.shouldDisplayYM2Warning(droit) ? 'needs-n-2' : '', droit.open ? 'panel-opened': ''].join(' ');
-    };
-});
