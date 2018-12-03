427baff7b33e35a4e921a1b2cffc45f1f9d1fd99
Thomas Guillet <guillet.thomas@gmail.com>
2018-12-03 17:09:23 +0100

    Fix linter errors

--- backend/lib/openfisca/bulk/index.js
+++ backend/lib/openfisca/bulk/index.js
@@ -10,77 +10,73 @@ var Situation = mongoose.model('Situation');
 
 var lib = require('..');
 
-var counter = 0;
-var errors = 0;
 var limit = 2000;
-var startDate = (new Date()).toISOString();
 
-
-entityGroups = {
-  individus: [],
-  familles: ['parents', 'enfants'],
-  foyers_fiscaux: ['declarants', 'personnes_a_charge'],
-  menages: ['personne_de_reference', 'conjoint', 'enfants']
-}
+var entityGroups = {
+    individus: [],
+    familles: ['parents', 'enfants'],
+    foyers_fiscaux: ['declarants', 'personnes_a_charge'],
+    menages: ['personne_de_reference', 'conjoint', 'enfants']
+};
 
 function reset() {
-  result = Object.keys(entityGroups).reduce((accum, entityName) => {
-    accum[entityName] = {};
-    return accum;
-  }, {})
-  result['individus'] = {};
+    var result = Object.keys(entityGroups).reduce(function (accum, entityName) {
+        accum[entityName] = {};
+        return accum;
+    }, {});
+    result['individus'] = {};
 
-  return result;
+    return result;
 }
 
 function prefix(prefix, situation) {
-  Object.keys(entityGroups).forEach(entityName => {
-    oldKeys = Object.keys(situation[entityName]);
-    oldKeys.forEach(name => {
-      situation[entityName][prefix + name] = situation[entityName][name]
-      delete situation[entityName][name]
-    })
+    Object.keys(entityGroups).forEach(function(entityName) {
+        var oldKeys = Object.keys(situation[entityName]);
+        oldKeys.forEach(function(name) {
+            situation[entityName][prefix + name] = situation[entityName][name];
+            delete situation[entityName][name];
+        });
 
-    entityGroups[entityName].forEach(property => {
-      Object.keys(situation[entityName]).forEach(id => {
-        let entity = situation[entityName][id]
+        entityGroups[entityName].forEach(function(property) {
+            Object.keys(situation[entityName]).forEach(function(id) {
+                var entity = situation[entityName][id];
 
-        if (entity[property]) {
-          entity[property] =  entity[property].map(id => prefix + id)
-        }
-      })
-    })
-  })
+                if (entity[property]) {
+                    entity[property] =  entity[property].map(function(id) { return prefix + id; });
+                }
+            });
+        });
+    });
 
-  return situation
+    return situation;
 }
 
 function append(acummulator, situation) {
-  Object.keys(entityGroups).forEach(entityName => {
-    Object.keys(situation[entityName]).forEach(id => {
-      acummulator[entityName][id] = situation[entityName][id]
-    })
-  })
+    Object.keys(entityGroups).forEach(function(entityName) {
+        Object.keys(situation[entityName]).forEach(function(id) {
+            acummulator[entityName][id] = situation[entityName][id];
+        });
+    });
 
-  return acummulator
+    return acummulator;
 }
 
-result = reset()
+var result = reset();
 function test() {
-  Situation.find().sort({ dateDeValeur: -1 }).skip(100).limit(limit).cursor()
-    .pipe(es.map(function (situation, done) {
-      append(result, prefix(situation._id.valueOf(), lib.buildOpenFiscaRequest(situation)))
-      done()
-    }))
-    .on('end', function() {
-      console.log(JSON.stringify(result))
-      process.exit();
-    })
-    .on('error', function(err) {
-      console.trace(err);
-      process.exit();
-    })
-    .resume();
+    Situation.find().sort({ dateDeValeur: -1 }).skip(100).limit(limit).cursor()
+        .pipe(es.map(function (situation, done) {
+            append(result, prefix(situation._id.valueOf(), lib.buildOpenFiscaRequest(situation)));
+            done();
+        }))
+        .on('end', function() {
+            console.log(JSON.stringify(result));
+            process.exit();
+        })
+        .on('error', function(err) {
+            console.trace(err);
+            process.exit();
+        })
+        .resume();
 }
 
 test();
